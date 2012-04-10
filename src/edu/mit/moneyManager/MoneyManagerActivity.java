11632b5d14a6692d1deed4185171bf6ebc990ca0
Qian Long <hungryha@gmail.com>
2012-04-09 21:47:42 -0400

    created android project

---
+++ src/edu/mit/moneyManager/MoneyManagerActivity.java
@@ -0,0 +1,13 @@
+package edu.mit.moneyManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoneyManagerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
