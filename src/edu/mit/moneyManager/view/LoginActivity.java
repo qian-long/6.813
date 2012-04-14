44a09491ab7c929bf2a6223138efa0aa8d302b3f
Qian Long <hungryha@gmail.com>
2012-04-13 21:58:10 -0400

    hides password chars when password is inputted

--- src/edu/mit/moneyManager/view/LoginActivity.java
+++ src/edu/mit/moneyManager/view/LoginActivity.java
@@ -4,8 +4,10 @@ import edu.mit.moneyManager.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * This activity is where the user logs in.
@@ -21,7 +23,9 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login);
         
         Button buttonLogin = (Button) findViewById(R.id.login_button);
-        
+        EditText username = (EditText) findViewById(R.id.login_username);
+        username.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             
             @Override
