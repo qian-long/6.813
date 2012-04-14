8397f1653ef26014be29ac5ff526a1d08a5a50e6
Qian Long <hungryha@gmail.com>
2012-04-13 21:03:08 -0400

    made login button navigate to home screen

--- src/edu/mit/moneyManager/view/LoginActivity.java
+++ src/edu/mit/moneyManager/view/LoginActivity.java
@@ -2,7 +2,10 @@ package edu.mit.moneyManager.view;
 
 import edu.mit.moneyManager.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * This activity is where the user logs in.
@@ -16,5 +19,18 @@ public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        
+        Button buttonLogin = (Button) findViewById(R.id.login_button);
+        
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(v.getContext(), HomeActivity.class);
+                startActivity(intent);
+                
+            }
+        });
     }
 }
