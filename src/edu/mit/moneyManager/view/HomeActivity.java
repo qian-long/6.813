4719a877285139c7afd35c81e74cb42a75f8c525
Stephanie Chang <niwen.chang@gmail.com>
2012-04-15 09:42:10 -0400

    tabs

--- src/edu/mit/moneyManager/view/HomeActivity.java
+++ src/edu/mit/moneyManager/view/HomeActivity.java
@@ -1,10 +1,12 @@
 package edu.mit.moneyManager.view;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 import edu.mit.moneyManager.R;
 
 /**
@@ -14,15 +16,42 @@ import edu.mit.moneyManager.R;
  * 
  * Returning users can view their budget and view budgets shared with them.
  */
-public class HomeActivity extends Activity {
+public class HomeActivity extends TabActivity {
     public static final boolean NEW = true;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost();  // The activity TabHost
+        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+        Intent intent;  // Reusable Intent for each tab
+
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent().setClass(this, ExpenseActivity.class);
+
+        // Initialize a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("artists").setIndicator("Artists")
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        // Do the same for the other tabs
+        intent = new Intent().setClass(this, ViewCategoryActivity.class);
+        spec = tabHost.newTabSpec("albums").setIndicator("Albums")
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        intent = new Intent().setClass(this, ViewChartActivity.class);
+        spec = tabHost.newTabSpec("songs").setIndicator("Songs",
+                          res.getDrawable(R.drawable.ic_launcher))
+                      .setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(0);
         
-        Button home = (Button) findViewById(R.id.home_action);
+        /*Button home = (Button) findViewById(R.id.home_action);
         Button view = (Button) findViewById(R.id.view_action);
         Button expenses = (Button) findViewById(R.id.expense_action);
         
@@ -51,7 +80,7 @@ public class HomeActivity extends Activity {
                 Intent intent = new Intent(v.getContext(), ExpenseActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
                 
     }
 }
