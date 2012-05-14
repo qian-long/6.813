233eb222e4fa64c57c02cc5ed74d14ba48c423f9
Qian Long <hungryha@gmail.com>
2012-05-14 12:45:03 -0400

    added total to chart

--- src/edu/mit/moneyManager/view/ViewChartActivity.java
+++ src/edu/mit/moneyManager/view/ViewChartActivity.java
@@ -2,86 +2,27 @@ package edu.mit.moneyManager.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import edu.mit.moneyManager.R;
 
 /**
  * Screen that shows chart breakdown of expenses.
- * 
  */
 public class ViewChartActivity extends Activity {
+    private static final String HEADER_TEXT = "Total Monthly Budget: $";
+    private static SharedPreferences settings;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_chart);
+        settings = getSharedPreferences(ViewSummaryActivity.PREFS_NAME, MODE_PRIVATE);
+        TextView total = (TextView) findViewById(R.id.total_budget);
+        total.setText(HEADER_TEXT + settings.getFloat(ViewSummaryActivity.BUDGET_TOTAL, 0));
         
-        //top actionbar
-//        Button home = (Button) findViewById(R.id.home_action);
-//        Button view = (Button) findViewById(R.id.view_action);
-//        Button expenses = (Button) findViewById(R.id.expense_action);
-//        
-//        home.setOnClickListener(new View.OnClickListener() {
-//            
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), HomeActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        view.setEnabled(false);
-//        view.setOnClickListener(new View.OnClickListener() {
-//            
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), ViewSummaryActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-        
-//        expenses.setOnClickListener(new View.OnClickListener() {
-//       
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), ExpenseActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-        
-        //view action bar
-//        Button summary = (Button) findViewById(R.id.summary_action);
-//        Button chart = (Button) findViewById(R.id.chart_action);
-//        Button edit = (Button) findViewById(R.id.edit_action);
-//        Button share = (Button) findViewById(R.id.share_action);
-//        
-//        chart.setEnabled(false);
-//        summary.setOnClickListener(new View.OnClickListener() {
-//            
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), ViewSummaryActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        
-//        edit.setOnClickListener(new View.OnClickListener() {
-//       
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), ViewEditBudgetActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        
-//        share.setOnClickListener(new View.OnClickListener() {
-//            
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(v.getContext(), ViewShareActivity.class);
-//                startActivity(intent);
-//            }
-//        });
     }
 }
